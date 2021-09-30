8b18a83f1146142798502c7c1f8d2be18df2267f
Rakesh Kumar Mallam <rmallam@redhat.com>
2021-09-30 11:10:20 +1000

    Updates

---
+++ precache-manifest.12d204324a87bcca01937ff808668a1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ab602ff415217e13574",
+    "url": "/aboutme/static/css/main.7e5b17f2.chunk.css"
+  },
+  {
+    "revision": "1ab602ff415217e13574",
+    "url": "/aboutme/static/js/main.1ab602ff.chunk.js"
+  },
+  {
+    "revision": "dc37bbc4bc435f4491ef",
+    "url": "/aboutme/static/js/runtime~main.dc37bbc4.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/aboutme/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e694956df7b5bdbeb68925fc1deea403",
+    "url": "/aboutme/index.html"
+  }
+];
